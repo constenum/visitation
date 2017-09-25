e27710fbc536a5aadd5183df237bbe516823a3af
Mark Miller <constenum@gmail.com>
2017-09-25 01:45:18 -0400

    add school routes and controller

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,30 @@ Route::name('student.destroy')->get('/student/{id}/delete', 'StudentController@d
 
 # Delete route to actually destroy the Student
 Route::name('student.destroy')->delete('/student/{id}', 'StudentController@destroy');
+
+/*
+* School resource
+*/
+# Index page to show all school
+Route::name('school.index')->get('/school', 'SchoolController@index');
+
+# Show form to create a new School
+Route::name('school.create')->get('/school/create', 'SchoolController@create');
+
+# Process the form to create a new School
+Route::name('school.store')->post('/school', 'SchoolController@store');
+
+# Show an individual School
+Route::name('school.show')->get('/school/{title}', 'SchoolController@show');
+
+# Show form to edit a School
+Route::name('school.edit')->get('/school/{id}/edit', 'SchoolController@edit');
+
+# Process form to edit a School
+Route::name('update')->put('/school/{id}', 'SchoolController@update');
+
+# Get route to confirm deletion of School
+Route::name('school.destroy')->get('/school/{id}/delete', 'SchoolController@delete');
+
+# Delete route to actually destroy the School
+Route::name('school.destroy')->delete('/school/{id}', 'SchoolController@destroy');
